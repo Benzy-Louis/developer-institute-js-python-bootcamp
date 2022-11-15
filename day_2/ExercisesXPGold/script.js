54a4eafc9ac324396da4ef6c9e9a6c90ec352d1f
Ruben Louis <louis.ruben@gmail.com>
2022-11-15 21:03:55 +0400

    work in progress

--- day_2/ExercisesXPGold/script.js
+++ day_2/ExercisesXPGold/script.js
@@ -1,10 +1,11 @@
 // Create 2 variables. The values should be strings. For example:
-// let str1 = "mix";
-// let str2 = "pod";
-
-
+let str1 = "mix";
+let str2 = "pod";
 // Slice out and swap the first 2 characters of the two strings from part 1.
-
+// let temp = str1.slice(0,2) ;
+// let temp_2 = str2.slice(0,2) ;
+// str1 = temp +
+consol
 
 // Create a third variable where the value is the concatenation of the two strings from the part 1 (separated by a space).
 
