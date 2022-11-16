9233510077a92c81322c5fc94b8de71450b12e0f
Ruben Louis <louis.ruben@gmail.com>
2022-11-16 13:23:09 +0400

    comments

--- week_1/day_3/ExercisesXP/exercise_1.js
+++ week_1/day_3/ExercisesXP/exercise_1.js
@@ -1,4 +1,5 @@
 /*
+Exercise 1: Simple If/Else Statement
 1. Create 2 variables, x and y. Each of them should have a different numeric value.
 2. Write an if/else statement that checks which number is bigger.
 
