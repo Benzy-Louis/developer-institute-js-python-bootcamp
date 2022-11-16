8e34ce6308499721027e089d062d3448710325e1
Ruben Louis <louis.ruben@gmail.com>
2022-11-16 13:03:40 +0400

    Exercises XP - Exercise 1

---
+++ week_1/day_3/ExercisesXP/exercise_1.js
@@ -0,0 +1,17 @@
+/*
+1. Create 2 variables, x and y. Each of them should have a different numeric value.
+2. Write an if/else statement that checks which number is bigger.
+
+let x = 5;
+let y = 2;
+
+You should display:
+x is the biggest number
+*/
+
+let x = 5;
+let y = 2;
+if (x > y) {
+    biggest = x;
+}
+console.log(`${biggest} is the biggest number among the numbers: ${x} and ${y}.`)
