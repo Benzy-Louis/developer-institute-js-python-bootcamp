d8134144bdb7533aba4077435bad14a510b05349
Znebur <louis.ruben@gmail.com>
2022-11-17 22:51:29 +0400

    Exercise 2 - XPGold

---
+++ week_1/day_3/ExerciseXPGold/exercise_2.js
@@ -0,0 +1,24 @@
+/*
+Exercise 2 : The Grade Assigner
+Instructions
+Ask the user for their grade.
+
+If the grade is bigger than 90, console.log "A"
+
+If the grade is between 80 and 90 (included), console.log "B"
+If the grade is between 70(included) and 80 (included), console.log "C"
+If the grade is lower than 70, console.log "D"
+*/
+
+userGrade = Number(prompt("Input your grade please."));
+
+if (userGrade > 90) {
+    console.log("A");
+} else if ((userGrade > 80) && (userGrade <= 90)) {
+    console.log("B");
+} else if ((userGrade >= 70) && (userGrade <= 80)) {
+    console.log("C");
+} else if ((userGrade >= 0) && (userGrade < 70)) {
+    console.log("D");
+}
+
