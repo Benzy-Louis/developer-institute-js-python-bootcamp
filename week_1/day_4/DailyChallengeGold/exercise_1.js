814f490ddbb7aff0efd2be7af7247300934b59f5
Znebur <louis.ruben@gmail.com>
2022-11-18 07:38:11 +0400

    Checking if the algo works properly

--- week_1/day_4/DailyChallengeGold/exercise_1.js
+++ week_1/day_4/DailyChallengeGold/exercise_1.js
@@ -15,8 +15,8 @@ Add comments and console.log the result for each step, this will help you unders
 Requirement: Don’t copy paste solutions from Google
 */
 
-const numbers = [5, 0, 9, 1, 7, 4, 2, 6, 3, 8];
-
+// const numbers = [5, 0, 9, 1, 7, 4, 2, 6, 3, 8];
+const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 console.log(numbers.toString());
 console.log(numbers.join());
 console.log(numbers.join("+"));
@@ -26,23 +26,16 @@ console.log(numbers.join(""));
 /*
 https://www.mygreatlearning.com/blog/bubble-sort/#bubble-sort-pseudocode
 Bubble Sort Pseudocode Descending
-1. We are given with an input array which is supposed to be sorted in descending order
-2. We start with the first element and i=0 index and check if the element present at i+1 is less then we swap the elements at index i and i+1.
-3. If above is not the case, then no swapping will take place.
-4. Now  " i " gets incremented and the above 2 steps happen again until the array is exhausted.
-5. We will ignore the last index as it is already sorted.
-6. Now the smallest element will be at the last index of the array.
-7. Now we will again set i=0 and continue with the same steps that will eventually place second smallest at second last place in the array. Now the last 2 indexes of the array are sorted.
-
 Bubble Sort Pseudocode in Descending order
 Bubble Sort(arr, size)
         for i=0 to n-i-1
-            for j=0 to n-i-2
+            for j=0 to n-i-1
                 if arr[j]<arr[j+1]
                     Swap arr[j] and arr[j+1] 
 */
-for (let i = 0; i < (numbers.length - i - 1); i++) {
-    for (let j = 0; j < (numbers.length - i - 1); j++) {
+for (let i = 0; i < (numbers.length - 1); i++) {
+    console.log(`i = ${i}`);
+    for (let j = 0; j < (numbers.length - 1); j++) {
         if (numbers[j] < numbers[j + 1]) {
             tempVar = numbers[j];
             numbers[j] = numbers[j + 1];
