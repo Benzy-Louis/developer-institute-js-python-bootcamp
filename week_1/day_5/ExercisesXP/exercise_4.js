47da7725f600c27d47e15e6304a08f879d8de036
Ruben Louis <louis.ruben@gmail.com>
2022-11-18 14:57:09 +0400

    exercise 4 - XP

--- week_1/day_5/ExercisesXP/exercise_4.js
+++ week_1/day_5/ExercisesXP/exercise_4.js
@@ -2,21 +2,24 @@
 /*
 Exercise 4 : Shopping List
 Instructions
+*/
 const stock = {
-    "banana": 6,
-    "apple": 0,
-    "pear": 12,
-    "orange": 32,
-    "blueberry":1
-}
+  'banana': 6,
+  'apple': 0,
+  'pear': 12,
+  'orange': 32,
+  'blueberry': 1,
+};
 
 const prices = {
-    "banana": 4,
-    "apple": 2,
-    "pear": 1,
-    "orange": 1.5,
-    "blueberry":10
-}
+  'banana': 4,
+  'apple': 2,
+  'pear': 1,
+  'orange': 1.5,
+  'blueberry': 10,
+};
+
+/*
 1. Add the stock and prices objects to your js file.
 
 2. Create an array called shoppingList with the following items: "banana", "orange", and "apple". It means that you have 1 banana, 1 orange and 1 apple in your cart.
@@ -24,10 +27,30 @@ const prices = {
 3. Create a function called myBill() that takes no parameters.
 
 4. The function should return the total price of your shoppingList. In order to do this you must follow these rules:
-    1. The item must be in stock. (Hint : check out if .. in)
-    2. If the item is in stock find out the price in the prices object.
+  1. The item must be in stock. (Hint : check out if .. in)
+  // It's not clear what should happen here
+  2. If the item is in stock find out the price in the prices object.
 
 5. Call the myBill() function.
 
 6. Bonus: If the item is in stock, decrease the item's stock by 1
 */
+
+const shoppingList = ['banana', 'orange', 'apple'];
+
+function myBill() {
+  let totalPriceOfShoppingList = 0;
+  // console.log(shoppingList);
+  for (const item of shoppingList ) {
+    // console.log(item);
+    if (stock[item] != 0) {
+      // console.log(item);
+      totalPriceOfShoppingList += prices[item];
+      stock[item] -= 1;
+    }
+  }
+  return totalPriceOfShoppingList;
+}
+
+console.log(myBill());
+console.log(stock);
