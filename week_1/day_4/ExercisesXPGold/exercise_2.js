b46fe5592c0f59e99eeb9262c30237a248961b9e
Znebur <louis.ruben@gmail.com>
2022-11-18 05:42:10 +0400

    Exercise 2 - XPGold

---
+++ week_1/day_4/ExercisesXPGold/exercise_2.js
@@ -0,0 +1,41 @@
+/*
+Exercise 2 : Attendance
+Instructions
+*/
+let guestList = {
+    randy: "Germany",
+    karla: "France",
+    wendy: "Japan",
+    norman: "England",
+    sam: "Argentina"
+}
+
+/*
+Given the object above where the key is the student’s name and the value is the country they are from.
+*/
+
+// 1. Prompt the student for their name.
+let studentName = prompt("What's your name, student?");
+// let studentName = "Wendy";
+// let studentName = "Chris";
+// let studentName = "Ruben";
+// let studentName = NaN;
+
+
+
+// 2. If the name is in the object, console.log the name of the student and the country they come from.
+
+/*
+For example: "Hi! I'm [name], and I'm from [country]."
+    -Hint: You don’t need to use a for loop, just look up the statement if ... in
+*/
+
+// 3. If the name is not in the object, console.log: "Hi! I'm a guest."
+
+if (((typeof studentName) === 'string')) {
+    if (studentName.toLowerCase() in guestList) {
+        console.log(`Hi! I'm ${studentName}, and I'm from from ${guestList[studentName.toLocaleLowerCase()]}.`);
+    } else {
+        console.log("Hi! I'm a guest.");
+    }
+}
