99dd1b383c5f608c76b79eaec985a5fcf7fa5211
Ruben Louis <louis.ruben@gmail.com>
2022-11-18 13:06:07 +0400

    Exercise 1 - XP

--- week_1/day_5/ExercisesXP/exercise_1.js
+++ week_1/day_5/ExercisesXP/exercise_1.js
@@ -7,13 +7,29 @@ Part I : function with no parameters
 1. Create a function called infoAboutMe() that takes no parameter.
 2. The function should console.log a sentence about you (ie. your name, age, hobbies ect…).
 3. Call the function.
+*/
+console.log('\nPart I : function with no parameters\n');
+function infoAboutMe() {
+  console.log('I\'m Ruben Louis, and I draw as a hobby.');
+}
 
+infoAboutMe();
 
+/*
 Part II : function with three parameters
 
 1. Create a function called infoAboutPerson(personName, personAge, personFavoriteColor) that takes 3 parameters.
-2. The function should console.log a sentence about the person (ie. “You name is …, you are .. years old, your favorite color is …”)
+2. The function should console.log a sentence about the person (ie. "You name is …, you are .. years old, your favorite color is …")
 3. Call the function twice with the following arguments:
 infoAboutPerson("David", 45, "blue")
 infoAboutPerson("Josh", 12, "yellow")
 */
+console.log('\nPart II : function with three parameters\n');
+
+function infoAboutPerson(personName, personAge, personFavoriteColor) {
+  console.log(`Your name is ${personName}, you are ${personAge} years old, your fabvorite color is ${personFavoriteColor}.`);
+}
+
+infoAboutPerson('David', 45, 'blue');
+infoAboutPerson('Josh', 12, 'yellow');
+
