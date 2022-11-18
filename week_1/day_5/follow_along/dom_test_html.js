b514aab60ace1d27a5dbc0b6f71694df38dc2712
Ruben Louis <louis.ruben@gmail.com>
2022-11-18 11:44:22 +0400

    Practice with DOM

---
+++ week_1/day_5/follow_along/dom_test_html.js
@@ -0,0 +1,10 @@
+// Get the div
+document.body.children[0];
+document.body.firstElementChild;
+
+// Get ul
+document.body.lastElementChild.previousElementSibling;
+
+// 2nd li insde the ul
+document.body.children[1].children[1];
+document.body.children[1].children[0].nextElementSibling;
