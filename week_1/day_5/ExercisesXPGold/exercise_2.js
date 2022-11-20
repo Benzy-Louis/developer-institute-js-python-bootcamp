72deaf71f50ceb66a9f4cbe1dee52b397ad3da65
Znebur <louis.ruben@gmail.com>
2022-11-20 13:13:27 +0400

    Ex 2. - XPGold

--- week_1/day_5/ExercisesXPGold/exercise_2.js
+++ week_1/day_5/ExercisesXPGold/exercise_2.js
@@ -5,3 +5,13 @@ Write a JavaScript function to convert a string into an abbreviated form.
 
 console.log(abbrevName("Robin Singh")); --> "Robin S."
 */
+function abbrevName(str) {
+  // Split on space the abbreviate other words
+  const arr = str.split(' ');
+  let abbrev = arr[0];
+  for (let i = 1; i < arr.length; i++) {
+    abbrev += ' ' + arr[i][0] + '.';
+  }
+  return abbrev;
+}
+console.log(abbrevName('Robin Singh'));
