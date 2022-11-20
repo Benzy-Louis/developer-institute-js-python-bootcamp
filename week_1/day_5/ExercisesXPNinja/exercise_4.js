533535e467aac54a564fd9bc1d217aaa8e80a43d
Znebur <louis.ruben@gmail.com>
2022-11-20 13:26:45 +0400

    Fix content

--- week_1/day_5/ExercisesXPNinja/exercise_4.js
+++ week_1/day_5/ExercisesXPNinja/exercise_4.js
@@ -1,42 +1,13 @@
 /* eslint-disable max-len */
 /* eslint-disable require-jsdoc */
 /*
-Exercise 2 : Grade Average
+Exercise 4 : Biggest Number
 Instructions
-Hint:
-- This Exercise is trickier then the others. You have to think about its structure before you start coding.
-- You must use functions to complete this exercise, to do so take a look at tomorrow’s lesson.
+Create a function called biggestNumberInArray(arrayNumber) that takes an array as a parameter and returns the biggest number.
+Note : This function should work with any array;
+Example:
 
-In this exercise we will be creating a function which takes an array of grades as an argument and will console.log the average.
-
-1. Create a function called findAvg(gradesList) that takes an argument called gradesList.
-
-2. Your function must calculate and console.log the average.
-
-3. If the average is above 65 let the user know they passed
-
-4. If the average is below 65 let the user know they failed and must repeat the course.
-Bonus Try and split parts 1,2 and 3,4 of this exercise to two separate functions.
-Hint One function must call the other.
+const array = [-1,0,3,100, 99, 2, 99] ;// should return 100 
+const array2 = ['a', 3, 4, 2]; // should return 4 
+const array3 = []; // should return 0
 */
-const grades = [20, 85, 90, 70, 80, 80];
-// let grades = [20, 40, 90, 3, 4, 5];
-
-function findAvg(gradesList) {
-  avg = 0;
-  for (grade of gradesList) {
-    avg += Number(grade);
-  }
-  avg /= gradesList.length;
-  console.log('Average grade: ' + avg.toFixed(1));
-  return avg;
-}
-
-function passCheck(avgGrade) {
-  if (avgGrade > 65) {
-    console.log('You passed!');
-  } else {
-    console.log('You failed, you have to repeat the course.');
-  }
-}
-passCheck(findAvg(grades));
