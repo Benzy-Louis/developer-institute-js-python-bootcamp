5fda0c11cc38dff8a6aa5035e616a4b0d38515df
Znebur <louis.ruben@gmail.com>
2022-11-20 12:41:50 +0400

    Ex1 - XPGold

--- week_1/day_5/ExercisesXPGold/exercise_1.js
+++ week_1/day_5/ExercisesXPGold/exercise_1.js
@@ -6,3 +6,8 @@ Write a program to check whether a string is blank or not.
 console.log(isBlank('')); --> true
 console.log(isBlank('abc')); --> false
 */
+function isBlank(str) {
+  return (str.length === 0);
+}
+console.log(isBlank(''));
+console.log(isBlank('abc'));
