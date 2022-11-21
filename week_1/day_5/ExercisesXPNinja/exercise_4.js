df478fe416b72b6881704bfb18d383fcc8fcdb87
Ruben Louis <louis.ruben@gmail.com>
2022-11-21 17:26:33 +0400

    Exercise 4 - XPNinja

--- week_1/day_5/ExercisesXPNinja/exercise_4.js
+++ week_1/day_5/ExercisesXPNinja/exercise_4.js
@@ -11,3 +11,26 @@ const array = [-1,0,3,100, 99, 2, 99] ;// should return 100
 const array2 = ['a', 3, 4, 2]; // should return 4
 const array3 = []; // should return 0
 */
+
+function biggestNumberInArray(arrayNumber) {
+  // Lazily check in a for loop which number is the biggest
+  let biggestNum = 0;
+  // if the array is empty return 0
+  if ( arrayNumber.length > 0) {
+    for ( let num of arrayNumber) {
+      num = Number(num);
+      // Check if the element is a number first
+      if ( !(isNaN(num))) {
+        // If so update the biggest number
+        if ( num > biggestNum ) {
+          biggestNum = num;
+        }
+      }
+    }
+    return biggestNum;
+  }
+}
+
+// console.log(biggestNumberInArray([]));
+// console.log(biggestNumberInArray(['a', 3, 4, 2]));
+console.log(biggestNumberInArray([-1, 0, 3, 100, 99, 2, 99]));
