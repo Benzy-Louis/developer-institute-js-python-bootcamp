21119dff75fa48ae0967225e2f79a3cd6ca3d4fb
Ruben Louis <louis.ruben@gmail.com>
2022-11-21 15:01:06 +0400

    Add print first and last character

--- week_2/day_1/DailyChallenge/exercise_1.py
+++ week_2/day_1/DailyChallenge/exercise_1.py
@@ -1,11 +1,11 @@
 # Instructions
-# Using the input function, ask the user for a string. The string must be 10 characters long.
+# 1. Using the input function, ask the user for a string. The string must be 10 characters long.
 # If it's less than 10 characters, print a message which states "string not long enough".
 # If it's more than 10 characters, print a message which states "string too long".
 
-# Then, print the first and last characters of the given text.
+# 2. Then, print the first and last characters of the given text.
 
-# Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed. For example:
+# 3. Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed. For example:
 # The user enters "Hello World"
 # Then, you have to construct the string character by character
 # H
@@ -23,6 +23,7 @@ import random
 user_string = input("Input a string of 10 characters please: ")
 if len(''.join(user_string.split())) == 10:
     display_string = ""
+    print(f"First character in string '{user_string[0]}' and last character in string is '{user_string[-1]}'")
     SHUFFLED_STRING = ''.join(random.sample(user_string, len(user_string)))
     for char in SHUFFLED_STRING:
         display_string += char
