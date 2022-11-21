65c423e33ead6694e3df2d91d8b5bd53038f567b
Ruben Louis <louis.ruben@gmail.com>
2022-11-21 14:05:48 +0400

    simplify and due to pylint suggestion

--- week_2/day_1/ExercisesGold/exercise_2.py
+++ week_2/day_1/ExercisesGold/exercise_2.py
@@ -15,10 +15,10 @@ while True:
     else:
         if user_month == 12 or user_month <= 2:
             print("That's in winter.")
-        elif user_month >= 3 and user_month <= 5:
+        elif 3 <= user_month <= 5:
             print("That's in spring.")
-        elif user_month >= 6 and user_month <= 8:
+        elif 6 <= user_month <= 8:
             print("That's in summer.")
-        elif user_month >= 9 and user_month <=11:
+        elif 9 <= user_month <=11:
             print("That's in autumn.")
         break
