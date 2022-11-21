1fd50fdf38ec1b069ad32bba53d6a6ea57aec94e
Ruben Louis <louis.ruben@gmail.com>
2022-11-21 15:42:54 +0400

    Day 5 - XPNinja

--- week_1/day_5/ExercisesXPNinja/exercise_1.js
+++ week_1/day_5/ExercisesXPNinja/exercise_1.js
@@ -4,3 +4,11 @@ Instructions
 Get a random number between 1 and 100.
 Console.log all even numbers from 0 to the random number.
 */
+const randomNumber = Math.floor(Math.random()*100 + 1);
+console.log(`The random number: ${randomNumber}`);
+
+for (let i = 0; i <= randomNumber; i++) {
+  if (i % 2 === 0) {
+    console.log(i);
+  }
+}
