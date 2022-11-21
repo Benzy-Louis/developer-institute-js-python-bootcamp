6cd838a128b448921ffca353fbe5d91170ad4fea
Ruben Louis <louis.ruben@gmail.com>
2022-11-21 16:54:20 +0400

    Exercise 3 - XPNinja

--- week_1/day_5/ExercisesXPNinja/exercise_3.js
+++ week_1/day_5/ExercisesXPNinja/exercise_3.js
@@ -6,3 +6,13 @@ Note A palindrome is a word, phrase or sequence that is spelled the same both
 backwards and forward, e.g., madam, bob or kayak.
 palindrome
 */
+
+function palindrome(str) {
+  // reverse the string and lazily check if it's the same
+  const reverseStr = str.split('').reverse().join('');
+  return reverseStr == str;
+}
+console.log(palindrome('madam'));
+console.log(palindrome('bob'));
+console.log(palindrome('kayak'));
+console.log(palindrome('orange'));
