55ae7d835c96a811fa08d84690a53c72b2dddb55
Ruben Louis <louis.ruben@gmail.com>
2022-11-21 10:53:15 +0400

    Exercise 3 - XP

---
+++ week_2/day_1/ExercisesXP/exercise_3.py
@@ -0,0 +1,8 @@
+# Exercise 3 : What Is The Output ?
+# Instructions
+# Predict the output of the following code snippets:
+# >>> 5 < 3 # False
+# >>> 3 == 3 # True
+# >>> 3 == "3" # False
+# >>> "3" > 3 # '>' not supported between instances of 'str' and 'int'
+# >>> "Hello" == "hello" # False
