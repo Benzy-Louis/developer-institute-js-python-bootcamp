884b2e12a0ea14a9a7afbe05804b88f514bed785
Ruben Louis <louis.ruben@gmail.com>
2022-11-22 20:00:25 +0400

    Ex. 1 - XP - Instructions

---
+++ week_2/day_2/ExercisesXP/exercise_1.py
@@ -0,0 +1,7 @@
+# Exercise 1 : Set
+# Instructions
+# Create a set called my_fav_numbers with all your favorites numbers.
+# Add two new numbers to the set.
+# Remove the last number.
+# Create a set called friend_fav_numbers with your friend’s favorites numbers.
+# Concatenate my_fav_numbers and friend_fav_numbers to a new variable called our_fav_numbers.
