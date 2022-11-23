13600a4d5e2e12abc5d2b33b8b94d4511ce01bab
Ruben Louis <louis.ruben@gmail.com>
2022-11-23 09:22:01 +0400

    Tutors suggestion use in

--- week_2/day_1/ExerciseXPNinja/exercise_5.py
+++ week_2/day_1/ExerciseXPNinja/exercise_5.py
@@ -4,13 +4,8 @@
 # Each time a user successfully sets a new longest sentence, print a congratulations message.
 
 while True:
-    user_string = input("Input the longest sentence you can without the character A: ")
-    char_a_in_string = False
-    for char in user_string:
-        # print(char)
-        if char.lower() == "a":
-            # print(char)
-            char_a_in_string = True
-    if char_a_in_string == False:
+    user_string = input(
+        "Input the longest sentence you can without the character A: ")
+    if "a" not in user_string.lower():
         print("Congratulations! you did input a long sentence without the character A.")
         break
